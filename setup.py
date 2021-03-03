82b60a417873958b469237770d3508b67243445b
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2021-03-03 09:43:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from distutils.core import setup, Extension
-setup(name = 'myModule', version = '1.0',  \
-   ext_modules = [Extension('myModule', ['main.cpp'])])
+setup(
+    name='sigTestC',
+    version='1.2',
+    author='Benjamin Blouin',
+    author_email='3keepmovingforward3@gmail.com',
+    url="https://github.com/3keepmovingforward3/SIG_Test",
+    description='Test given for SIG hiring',
+    ext_modules=[Extension('sigTestC', ['main.cpp'])])
